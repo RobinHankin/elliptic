1a7fb761087db1466650afd49525f14b79e44c0a
Robin Hankin <hankin.robin@gmail.com>
2021-08-17 11:17:04 +1200

    use elliptic_stickermaker.R for consistency

--- inst/hex_sticker_plotmaker.R
+++
@@ -1,4 +0,0 @@
-x <- seq(from=-4,to=4,len=800)
-cis <- function(theta){cos(theta)+1i*sin(theta)}
-z <- outer(x,1i*x,"+")*cis(-0.33)
-view(x,x,limit(zeta(z,c(1+1i,2-3i)),upper=10),nlevel=16,scheme=-1,real=TRUE,imag=FALSE,drawlabels=FALSE)
