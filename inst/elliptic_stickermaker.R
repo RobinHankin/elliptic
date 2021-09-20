f6897692546901a42f0de10c5fb36c7ebf1fa1d1
Robin Hankin <hankin.robin@gmail.com>
2021-09-20 12:01:40 +1200

    tweaks plus color consistentification

--- inst/elliptic_stickermaker.R
+++ inst/elliptic_stickermaker.R
@@ -33,15 +33,13 @@ dotted.lines <-
     g(0.9,0.0),NA,
     g(1.0,0.0),NA
     )
+
 png(file="elliptic_icon.png",width=1000,height=1000,bg="transparent")
-plot(P(z=solid.lines,params=p),xlim=c(-4,4),ylim=c(-6,3),type="l",lwd=7,asp=1,axes=FALSE,xlab="",ylab="")
-lines(P(z=dotted.lines,params=p),xlim=c(-4,4),ylim=c(-6,0),type="l",lty=2,lwd=7)
+plot(P(z=solid.lines,params=p),xlim=c(-4,4),ylim=c(-6,9),type="l",lwd=7,asp=1,axes=FALSE,xlab="",ylab="")
+lines(P(z=dotted.lines,params=p),type="l",lty=3,lwd=7)
 dev.off()
 
-
-
-
-sticker("elliptic_icon.png", package="elliptic", p_size=8, s_x=0.975, s_y=0.8,
-s_width=1.1,asp=0.85, white_around_sticker=TRUE, h_fill="#7733FF",
+sticker("elliptic_icon.png", package="elliptic", p_size=80, s_x=0.975, s_y=1.3,
+s_width=1.6,asp=sqrt(3)/2, white_around_sticker=TRUE, h_fill="#7733FF", dpi=1000,
 h_color="#000000", filename="elliptic.png")
 
